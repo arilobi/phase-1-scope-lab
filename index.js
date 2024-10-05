e279eaf13da929afe7708799e31e00afe4005dcf
Marion Nabulobi <arinabulobi@gmail.com>
2024-10-05 09:37:04 +0300

    add scope

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function customerName(){
+    return customerName;
+}
+console.log(window.customerName('bob'));
+
+function upperCaseCustomerName(){
+    customerName = customerName.upperCaseCustomerName()
+}
+console.log(window.customerName('BOB'));
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+console.log(bestCustomer);
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+console.log(bestCustomer);
+
+const leastFavoriteCustomer = "Jackie";
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "Sally";
+}
+console.log(changeLeastFavoriteCustomer('Assignment to constant variable'));
